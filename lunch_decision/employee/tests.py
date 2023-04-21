186765f759d8710cf331138f6a7ea8d254334b76
Nikola <mtsebrov@gmail.com>
2023-04-22 00:15:22 +0300

    rewrite Employee tests

--- lunch_decision/employee/tests.py
+++ lunch_decision/employee/tests.py
@@ -1,78 +1,58 @@
-import pytest
-from django.contrib.auth import get_user_model
-from rest_framework.test import APIClient
-from restaurant.models import Restaurant, Menu, Vote
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APITestCase
+from employee.models import Employee
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
-@pytest.fixture
-def api_client():
-    return APIClient()
+class EmployeeTests(APITestCase):
+    def setUp(self):
+        self.superuser = Employee.objects.create_superuser(
+            username="superuser",
+            email="superuser@example.com",
+            password="superpassword123",
+        )
+        self.user = Employee.objects.create(
+            username="user",
+            email="user@example.com",
+            password="password123",
+        )
+        user_refresh = RefreshToken.for_user(self.user)
+        self.user_access_token = str(user_refresh.access_token)
 
+        refresh = RefreshToken.for_user(self.superuser)
+        self.access_token = str(refresh.access_token)
 
-@pytest.fixture
-def employee_user():
-    user = get_user_model().objects.create_user(
-        username="employeeuser",
-        password="testpassword123",
-        is_restaurant=False,
-    )
-    return user
+    def test_user_create_employee(self):
+        url = reverse("create_employee")
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password": "testpassword123",
+            "first_name": "John",
+            "last_name": "Doe",
+            "is_restaurant": False,
+        }
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Bearer {self.user_access_token}"
+        )
+        response = self.client.post(url, data, format="json")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
-
-@pytest.fixture
-def restaurant_user():
-    user = get_user_model().objects.create_user(
-        username="restaurantuser",
-        password="testpassword123",
-        is_restaurant=True,
-    )
-    return user
-
-
-@pytest.fixture
-def restaurant(restaurant_user):
-    restaurant = Restaurant.objects.create(
-        employee=restaurant_user,
-        name="Test Restaurant",
-        address="123 Test St",
-        description="A test restaurant",
-    )
-    return restaurant
-
-
-@pytest.fixture
-def menu(restaurant):
-    menu = Menu.objects.create(
-        restaurant=restaurant,
-        date="2022-10-01",
-        items="Menu items for test",
-    )
-    return menu
-
-
-@pytest.fixture
-def vote(employee_user, menu):
-    vote = Vote.objects.create(
-        employee=employee_user,
-        menu=menu,
-        date="2022-10-01",
-    )
-    return vote
-
-
-# Test Employee creation
-def test_create_employee(api_client):
-    url = reverse("employee-create")
-    data = {
-        "username": "testuser",
-        "email": "testuser@example.com",
-        "password": "testpassword123",
-        "first_name": "John",
-        "last_name": "Doe",
-        "is_restaurant": False,
-    }
-    response = api_client.post(url, data, format="json")
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.data["username"] == "testuser"
+    def test_superuser_create_employee(self):
+        url = reverse("create_employee")
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password": "testpassword123",
+            "first_name": "John",
+            "last_name": "Doe",
+            "is_restaurant": False,
+        }
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
+        )
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()["username"], "testuser")
+        self.assertEqual(response.json()["is_restaurant"], False)
