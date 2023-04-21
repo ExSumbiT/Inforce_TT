9918d3bdd5902f513e0489cbcdbe48c27dbf6d40
Nikola <mts@3i.ua>
2023-04-21 16:27:04 +0300

    new urls for menu and votes

--- lunch_decision/lunch_service/urls.py
+++ lunch_decision/lunch_service/urls.py
@@ -1,4 +1,11 @@
-from .views import CreateEmployeeView, RestaurantCreateView
+from .views import (
+    CreateEmployeeView,
+    RestaurantCreateView,
+    MenuUploadView,
+    CurrentDayMenuView,
+    CurrentDayResultsView,
+    VoteCreateView,
+)
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,6 +18,10 @@ urlpatterns = [
     path(
         "create_restaurant/", RestaurantCreateView.as_view(), name="create_restaurant"
     ),
+    path("upload_menu/", MenuUploadView.as_view(), name="upload_menu"),
+    path("menus/today/", CurrentDayMenuView.as_view(), name="current_day_menu"),
+    path("vote/", VoteCreateView.as_view(), name="create_vote"),
+    path("results/today/", CurrentDayResultsView.as_view(), name="current_day_results"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
